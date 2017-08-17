c0c45df81cf7374e3173f73c95c283127abda3ae
Ronald Gunawan R <geronald10@gmail.com>
2017-08-17 23:06:42 +0700

    add auto sinkron logic for menu verifikasi tandatangan and menu verifikasi wajah + UI revision

--- app/src/main/java/id/ac/its/sikemastc/activity/verifikasi_wajah/VerifikasiWajahMenuActivity.java
+++ app/src/main/java/id/ac/its/sikemastc/activity/verifikasi_wajah/VerifikasiWajahMenuActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
@@ -14,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,6 +49,9 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
 
     private SharedPreferences flagStatus;
     private Button btnVerifikasiWajah;
+    private Button btnKelolaDataSetWajah;
+    private ImageView ivStatusData;
+    private TextView tvStatusData;
     private Context mContext;
     private Toolbar toolbar;
     private String nrpMahasiswa;
@@ -54,7 +59,7 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
     private String idPerkuliahan;
     private String userTerlogin;
     private SikemasSessionManager session;
-    private ArrayList<String> imageUrlList, fileUrlList;
+    private ArrayList<String> fileUrlList;
     private ProgressDialog progressDialog;
     private FileHelper fh;
 
@@ -66,7 +71,7 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
 
         mContext = this;
         session = new SikemasSessionManager(this);
-        flagStatus = getSharedPreferences("flag_status", 0);
+        flagStatus = getSharedPreferences("status_file_flag", 0);
 
         // Progress Dialog
         progressDialog = new ProgressDialog(this);
@@ -97,6 +102,7 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
         namaMahasiswa = userDetail.get(SikemasSessionManager.KEY_USER_NAME);
         idPerkuliahan = intent.getStringExtra("id_perkuliahan");
         userTerlogin= nrpMahasiswa + " - " + namaMahasiswa;
+
         String training = intent.getStringExtra("training");
         if (training != null && !training.isEmpty()) {
             Toast.makeText(getApplicationContext(), training, Toast.LENGTH_SHORT).show();
@@ -104,15 +110,17 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
         }
 
         TextView tvUserTerlogin = (TextView) findViewById(R.id.tv_user_detail);
-        tvUserTerlogin.setText(nrpMahasiswa + " - " + namaMahasiswa);
+        tvUserTerlogin.setText(userTerlogin);
 
-        Button btnKelolaDataSetWajah = (Button) findViewById(R.id.btn_kelola_data_set_wajah);
+        btnKelolaDataSetWajah = (Button) findViewById(R.id.btn_kelola_data_set_wajah);
         btnVerifikasiWajah = (Button) findViewById(R.id.btn_verification_view);
+        ivStatusData = (ImageView) findViewById(R.id.iv_status_data_wajah);
+        tvStatusData = (TextView) findViewById(R.id.tv_status_data_wajah);
 
         btnKelolaDataSetWajah.setOnClickListener(operate);
         btnVerifikasiWajah.setOnClickListener(operate);
 
-        checkDataSetStatus();
+        downloadFileFromUrl();
     }
 
     View.OnClickListener operate = new View.OnClickListener() {
@@ -121,18 +129,11 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
             switch (v.getId()) {
                 case R.id.btn_kelola_data_set_wajah:
                     downloadFileFromUrl();
-//                    if (getJumlahDataSetClient() < 20) {
-////                        sinkronisasiDataSet(nrpMahasiswa);
-//                    } else {
-//                        Toast.makeText(getApplicationContext(), "Data Wajah Ditemukan Sama Dengan Data Wajah di Server",
-//                                Toast.LENGTH_SHORT).show();
-//                    }
                     break;
-
                 case R.id.btn_verification_view:
-                    if (flagStatus.getBoolean("training_flag", false)) {
+                    if (flagStatus.getBoolean("status_file_flag", false)) {
                         Toast.makeText(getApplication(), "Fitur Verifikasi Kehadiran tidak dapat " +
-                                        "digunakan\nFitur ini aktif ketika data wajah telah ditraining sebelumnya",
+                                        "digunakan\nFitur ini aktif ketika data telah berhasil diunduh",
                                 Toast.LENGTH_LONG).show();
                     } else {
                         Intent intentToRecognition = new Intent(v.getContext(), VerifikasiWajahActivity.class);
@@ -146,137 +147,19 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
         }
     };
 
-    // Get jumlah data set wajah di local file system
-    private int getJumlahDataSetClient() {
-        FileHelper imageFiles = new FileHelper(userTerlogin);
-        File[] imageList = imageFiles.getTrainingList();
-        return imageList.length;
-    }
-
-    // Download imageUrl from server
-    private void sinkronisasiDataSet(final String userId) {
-        //Showing the progress dialog
-        imageUrlList = new ArrayList<>();
-        progressDialog.setMessage("Pengecekan Data Wajah di Server... ");
-        progressDialog.show();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, NetworkUtils.LIST_FOTO_MAHASISWA_IMAGE_URL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d(TAG, "get image url " + response);
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            JSONArray list = jsonObject.getJSONArray("list");
-                            if (list.length() > 0) {
-                                for (int i = 0; i < list.length(); i++) {
-                                    JSONObject urlImage = list.getJSONObject(i);
-                                    String fotoUrl = urlImage.getString("foto").replace(" ", "%20");
-                                    imageUrlList.add(fotoUrl);
-                                    Log.d("imageUrl", imageUrlList.get(i));
-                                }
-                                progressDialog.dismiss();
-                                downloadImageFromUrl();
-                                Toast.makeText(VerifikasiWajahMenuActivity.this, "Data Set ditemukan",
-                                        Toast.LENGTH_SHORT).show();
-                            } else {
-                                progressDialog.dismiss();
-                                Toast.makeText(VerifikasiWajahMenuActivity.this, "Data Set tidak ditemukan, " +
-                                        "Anda belum mendaftarkan Foto Wajah Anda", Toast.LENGTH_SHORT).show();
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        // Dismissing the progress dialog
-                        progressDialog.dismiss();
-                        Log.d("VolleyErroyResponse", "Error");
-                        //Showing toast
-                        Toast.makeText(VerifikasiWajahMenuActivity.this, volleyError.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                // Get encoded Image
-                Map<String, String> params = new HashMap<>();
-                params.put("nrp", userId);
-                //returning parameters
-                return params;
-            }
-        };
-        VolleySingleton.getmInstance(getApplicationContext()).addToRequestQueue(stringRequest);
-    }
-
-    // Download image from imageUrl
-    private void downloadImageFromUrl() {
-        //Showing the progress dialog
-        progressDialog.setMessage("Mengunduh Data Wajah... ");
-        progressDialog.show();
-
-        for (int i = 0; i < imageUrlList.size(); i++) {
-            final int index = i;
-            Log.d("image URL" + index, imageUrlList.get(i));
-            InputStreamVolleyRequest inputStreamRequest = new InputStreamVolleyRequest(Request.Method.GET, imageUrlList.get(i),
-                    new Response.Listener<byte[]>() {
-                        @Override
-                        public void onResponse(byte[] response) {
-                            try {
-                                Log.d("Sinkronisasi Volley", "response");
-                                if (response != null) {
-                                    String wholeFolderPath = fh.TRAINING_PATH + userTerlogin;
-                                    new File(wholeFolderPath).mkdirs();
-                                    String name = userTerlogin + "_" + index + ".png";
-                                    String fullpath = wholeFolderPath + "/" + name;
-                                    File outputFile = new File(fullpath);
-
-                                    FileOutputStream outputStream = new FileOutputStream(outputFile);
-                                    outputStream.write(response);
-                                    outputStream.close();
-                                    if (index + 1 == imageUrlList.size()) {
-                                        flagStatus.edit().putBoolean("training_flag", false).apply();
-                                        flagStatus.edit().putBoolean("upload_flag", true).apply();
-                                        progressDialog.dismiss();
-                                        Toast.makeText(getApplicationContext(), "Berhasil mengunduh data wajah",
-                                                Toast.LENGTH_SHORT).show();
-                                        runOnUiThread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                            }
-                                        });
-                                    }
-                                }
-                            } catch (Exception e) {
-                                Log.d("KEY_ERROR", "UNABLE TO DOWNLOAD FILE");
-                                e.printStackTrace();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-                            // Dismissing the progress dialog
-                            progressDialog.dismiss();
-                            Log.d("VolleyErroyResponse", "Error");
-                            //Showing toast
-                            Toast.makeText(VerifikasiWajahMenuActivity.this, volleyError.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    }, null);
-            VolleySingleton.getmInstance(getApplicationContext()).addToRequestQueue(inputStreamRequest);
-        }
-    }
-
     private void checkDataSetStatus() {
-        boolean flag = flagStatus.getBoolean("training_flag", false);
+        boolean flag = flagStatus.getBoolean("status_file_flag", false);
         if (!flag) {
+            btnKelolaDataSetWajah.setVisibility(View.VISIBLE);
+            ivStatusData.setImageResource(R.drawable.ic_error_outline);
+            tvStatusData.setText("data training tidak ditemukan");
             btnVerifikasiWajah.setCompoundDrawablesWithIntrinsicBounds(
                     ContextCompat.getDrawable(mContext, R.drawable.ic_error_outline), null, null, null);
             btnVerifikasiWajah.setTextColor(ContextCompat.getColor(mContext, R.color.colorSecondaryText));
         } else {
+            btnKelolaDataSetWajah.setVisibility(View.GONE);
+            ivStatusData.setImageResource(R.drawable.ic_verified_user);
+            tvStatusData.setText("data training ditemukan");
             btnVerifikasiWajah.setCompoundDrawablesWithIntrinsicBounds(
                     ContextCompat.getDrawable(mContext, R.drawable.ic_verified_user), null, null, null);
             btnVerifikasiWajah.setTextColor(ContextCompat.getColor(mContext, R.color.colorPrimaryText));
@@ -286,7 +169,7 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
     // Download file from fileUrl
     private void downloadFileFromUrl() {
         //Showing the progress dialog
-        progressDialog.setMessage("Mengunduh Data Wajah... ");
+        progressDialog.setMessage("Mengunduh Data ... ");
         progressDialog.show();
 
         fileUrlList = new ArrayList<>();
@@ -319,7 +202,7 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
                                     outputStream.close();
 
                                     if (index + 1 == fileUrlList.size()) {
-                                        flagStatus.edit().putBoolean("training_flag", false).apply();
+                                        flagStatus.edit().putBoolean("status_file_flag", true).apply();
                                         checkDataSetStatus();
                                         progressDialog.dismiss();
                                         Toast.makeText(getApplicationContext(), "Berhasil mengunduh data training",
@@ -329,7 +212,8 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
                                             public void run() {
                                             }
                                         });
-                                    }
+                                    } else
+                                        flagStatus.edit().putBoolean("status_file_flag", false).apply();
                                 }
                             } catch (Exception e) {
                                 Log.d("KEY_ERROR", "UNABLE TO DOWNLOAD FILE");
@@ -349,11 +233,5 @@ public class VerifikasiWajahMenuActivity extends AppCompatActivity {
                     }, null);
             VolleySingleton.getmInstance(getApplicationContext()).addToRequestQueue(inputStreamRequest);
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        checkDataSetStatus();
     }
 }
